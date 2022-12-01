893e38c6b23cd874918b4d5a53b86a3d9a6542aa
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-01 17:26:43 +0100

    Solve day 1 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day01.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace AdventOfCode._2022;
+
+public static class Day01
+{
+    public static int CountCaloriesPart1(string input)
+    {
+        var sb = new StringBuilder();
+        var i = 0;
+        var max = 0;
+        var currentElfSum = 0;
+
+        while (i < input.Length)
+        {
+            if (input[i..(i + 4)] == "\r\n\r\n")
+            {
+                max = currentElfSum > max ? currentElfSum : max;
+                currentElfSum = 0;
+                i += 4;
+            }
+            else
+            {
+                if (input[i..(i + 2)] == "\r\n")
+                {
+                    i += 2;
+                }
+
+                while (i < input.Length && input[i] != '\r')
+                {
+                    sb.Append(input[i++]);
+                }
+
+                currentElfSum += int.Parse(sb.ToString());
+                sb.Clear();
+            }
+        }
+
+        return max;
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day01Tests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode._2022;
+using Xunit;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day01Tests
+{
+    [Fact]
+    public void CountCalories_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "1000\r\n2000\r\n3000\r\n\r\n4000\r\n\r\n5000\r\n6000\r\n\r\n7000\r\n8000\r\n9000\r\n\r\n10000";
+        const int expectedResult = 24000;
+
+        // Act
+        var result = Day01.CountCaloriesPart1(input);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
