fdeb63c2c653ec581808ab36c5feb442564a019b
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-01 17:39:37 +0100

    Solve day 1 part 2

--- AdventOfCode/AdventOfCode/2022/Day01.cs
+++ AdventOfCode/AdventOfCode/2022/Day01.cs
@@ -38,4 +38,68 @@ public static class Day01
 
         return max;
     }
+
+    public static int CountCaloriesPart2(string input)
+    {
+        var sb = new StringBuilder();
+        var i = 0;
+        int max1 = 0, max2 = 0, max3 = 0;
+        var currentElfSum = 0;
+
+        while (i < input.Length)
+        {
+            if (input[i..(i + 4)] == "\r\n\r\n")
+            {
+                if (currentElfSum > max1)
+                {
+                    max3 = max2;
+                    max2 = max1;
+                    max1 = currentElfSum;
+                }
+                else if (currentElfSum > max2)
+                {
+                    max3 = max2;
+                    max2 = currentElfSum;
+                }
+                else if (currentElfSum > max3)
+                {
+                    max3 = currentElfSum;
+                }
+                currentElfSum = 0;
+                i += 4;
+            }
+            else
+            {
+                if (input[i..(i + 2)] == "\r\n")
+                {
+                    i += 2;
+                }
+
+                while (i < input.Length && input[i] != '\r')
+                {
+                    sb.Append(input[i++]);
+                }
+
+                currentElfSum += int.Parse(sb.ToString());
+                sb.Clear();
+            }
+        }
+
+        if (currentElfSum > max1)
+        {
+            return currentElfSum + max1 + max2;
+        }
+
+        if (currentElfSum > max2)
+        {
+            return max1 + currentElfSum + max2;
+        }
+
+        if (currentElfSum > max3)
+        {
+            return max1 + max2 + currentElfSum;
+        }
+
+        return max1 + max2 + max3;
+    }
 }
--- AdventOfCode/AdventOfCodeTests/2022/Day01Tests.cs
+++ AdventOfCode/AdventOfCodeTests/2022/Day01Tests.cs
@@ -6,14 +6,28 @@ namespace AdventOfCodeTests._2022;
 public class Day01Tests
 {
     [Fact]
-    public void CountCalories_ShouldReturnExpectedResult()
+    public void CountCaloriesPart1_ShouldReturnExpectedResult()
     {
         // Arrange
         var input = "1000\r\n2000\r\n3000\r\n\r\n4000\r\n\r\n5000\r\n6000\r\n\r\n7000\r\n8000\r\n9000\r\n\r\n10000";
-        const int expectedResult = 24000;
+        const int expectedResult = 24_000;
 
         // Act
         var result = Day01.CountCaloriesPart1(input);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void CountCaloriesPart2_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "1000\r\n2000\r\n3000\r\n\r\n4000\r\n\r\n5000\r\n6000\r\n\r\n7000\r\n8000\r\n9000\r\n\r\n10000";
+        const int expectedResult = 45_000;
+
+        // Act
+        var result = Day01.CountCaloriesPart2(input);
 
         // Assert
         Assert.Equal(expectedResult, result);
