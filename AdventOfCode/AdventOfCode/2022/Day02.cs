0c9f74bb73c39d455461df8bf7b685bb63a1bfa6
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-02 17:46:12 +0100

    Solve day 2 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day02.cs
@@ -0,0 +1,36 @@
+﻿
+namespace AdventOfCode._2022;
+
+public static class Day02
+{
+    // Scoring:
+    // 1 for Rock (A, X)
+    // 2 for Paper (B, Y)
+    // 3 for Scissors (C, Z)
+    // +0 for lose
+    // +3 for draw
+    // +6 for win
+    private static Dictionary<string, int> RoundToScoreMap = new()
+    {
+        {"A X", 1 + 3},
+        {"A Y", 2 + 6},
+        {"A Z", 3 + 0},
+        {"B X", 1 + 0},
+        {"B Y", 2 + 3},
+        {"B Z", 3 + 6},
+        {"C X", 1 + 6},
+        {"C Y", 2 + 0},
+        {"C Z", 3 + 3},
+    };
+
+    public static int RockPaperScissorsPart1(string input)
+    {
+        var score = 0;
+        for (int i = 0; i < input.Length; i += 5)
+        {
+            score += RoundToScoreMap[input[i..(i + 3)]];
+        }
+
+        return score;
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day02Tests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using AdventOfCode._2022;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day02Tests
+{
+    [Fact]
+    public void RockPaperScissorsPart1_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "A Y\r\nB X\r\nC Z";
+        
+        const int expectedResult = 15;
+
+        // Act
+        var score = Day02.RockPaperScissorsPart1(input);
+
+        // Assert
+        Assert.Equal(expectedResult, score);
+    }
+}
