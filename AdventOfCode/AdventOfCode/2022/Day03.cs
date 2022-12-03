79cfed56b41025563f4a39233688c100f54e8823
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-03 16:08:20 +0100

    Solve day 3 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day03.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace AdventOfCode._2022;
+
+public static class Day03
+{
+    public static int RucksackReorganization(string input)
+    {
+        var i = 0;
+        var lines = input.Split("\r\n");
+        var prioritiesSum = 0;
+        foreach (var line in lines)
+        {
+            var c1 = line[..(line.Length / 2)];
+            var c2 = line[(line.Length / 2)..];
+            var c1CharMap = new Dictionary<char, int>();
+
+            foreach (var ch1 in c1)
+            {
+                if (c1CharMap.ContainsKey(ch1))
+                {
+                    c1CharMap[ch1]++;
+                }
+                else
+                {
+                    c1CharMap[ch1] = 1;
+                }
+            }
+
+            var recurringItem = ' ';
+            foreach (var ch2 in c2)
+            {
+                if (c1CharMap.ContainsKey(ch2))
+                {
+                    if (char.IsLower(ch2))
+                    {
+                        prioritiesSum += ch2 - 96;
+                    }
+                    else
+                    {
+                        prioritiesSum += ch2 - 38;
+                    }
+
+                    break;
+                }
+            }
+
+        }
+        return prioritiesSum;
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day03Tests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode._2022;
+using Xunit;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day03Tests
+{
+    [Fact]
+    public void RucksackReorganizationPart1_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "vJrwpWtwJgWrhcsFMMfFFhFp\r\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\r\nPmmdzqPrVvPwwTWBwg\r\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\r\nttgJtRGJQctTZtZT\r\nCrZsJsPPZsGzwwsLwLmpwMDw";
+        const int expectedPrioritySum = 157;
+
+        // Act
+        var result = Day03.RucksackReorganization(input);
+
+        // Assert
+        Assert.Equal(expectedPrioritySum, result);
+    }
+}
