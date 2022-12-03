bb418c2bfaa72b26e9a48987e916d0c40949185a
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-03 16:26:25 +0100

    Solve day 3 part 2

--- AdventOfCode/AdventOfCode/2022/Day03.cs
+++ AdventOfCode/AdventOfCode/2022/Day03.cs
@@ -1,12 +1,9 @@
-﻿using System.Text;
-
-namespace AdventOfCode._2022;
+﻿namespace AdventOfCode._2022;
 
 public static class Day03
 {
-    public static int RucksackReorganization(string input)
+    public static int RucksackReorganizationPart1(string input)
     {
-        var i = 0;
         var lines = input.Split("\r\n");
         var prioritiesSum = 0;
         foreach (var line in lines)
@@ -17,17 +14,9 @@ public static class Day03
 
             foreach (var ch1 in c1)
             {
-                if (c1CharMap.ContainsKey(ch1))
-                {
-                    c1CharMap[ch1]++;
-                }
-                else
-                {
-                    c1CharMap[ch1] = 1;
-                }
+                c1CharMap[ch1] = 1;
             }
 
-            var recurringItem = ' ';
             foreach (var ch2 in c2)
             {
                 if (c1CharMap.ContainsKey(ch2))
@@ -46,6 +35,53 @@ public static class Day03
             }
 
         }
+        return prioritiesSum;
+    }
+
+    public static int RucksackReorganizationPart2(string input)
+    {
+        var lines = input.Split("\r\n");
+        var groups = new List<List<string>>();
+        for (int i = 0; i < lines.Length / 3; i++)
+        {
+            groups.Add(lines.Skip(i * 3).Take(3).ToList());
+        }
+        var prioritiesSum = 0;
+        foreach (var group in groups)
+        {
+                var c1 = group[0].ToCharArray();
+                var c2 = group[1].ToCharArray();
+                var c1CharMap = new Dictionary<char, int>();
+                var c2CharMap = new Dictionary<char, int>();
+
+                foreach (var ch1 in c1)
+                {
+                    c1CharMap[ch1] = 1;
+                }
+
+                foreach (var ch2 in c2)
+                {
+                    c2CharMap[ch2] = 1;
+                }
+
+                foreach (var ch3 in group[2].ToCharArray())
+                {
+                    if (c1CharMap.ContainsKey(ch3) && c2CharMap.ContainsKey(ch3))
+                    {
+                        if (char.IsLower(ch3))
+                        {
+                            prioritiesSum += ch3 - 96;
+                        }
+                        else
+                        {
+                            prioritiesSum += ch3 - 38;
+                        }
+
+                        break;
+                    }
+                }
+        }
+        
         return prioritiesSum;
     }
 }
--- AdventOfCode/AdventOfCodeTests/2022/Day03Tests.cs
+++ AdventOfCode/AdventOfCodeTests/2022/Day03Tests.cs
@@ -13,7 +13,21 @@ public class Day03Tests
         const int expectedPrioritySum = 157;
 
         // Act
-        var result = Day03.RucksackReorganization(input);
+        var result = Day03.RucksackReorganizationPart1(input);
+
+        // Assert
+        Assert.Equal(expectedPrioritySum, result);
+    }
+
+    [Fact]
+    public void RucksackReorganizationPart2_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "vJrwpWtwJgWrhcsFMMfFFhFp\r\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\r\nPmmdzqPrVvPwwTWBwg\r\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\r\nttgJtRGJQctTZtZT\r\nCrZsJsPPZsGzwwsLwLmpwMDw";
+        const int expectedPrioritySum = 70;
+
+        // Act
+        var result = Day03.RucksackReorganizationPart2(input);
 
         // Assert
         Assert.Equal(expectedPrioritySum, result);
