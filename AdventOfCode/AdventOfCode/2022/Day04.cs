340e45b4feaab5803f35fd86b0e94a9a0c43d749
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-04 21:07:39 +0100

    Solve day 4 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day04.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode._2022;
+
+public static class Day04
+{
+    public static int CampCleanupPart1(string input)
+    {
+        var lines = input.Split("\r\n");
+        var containedCount = 0;
+
+        foreach (var line in lines)
+        {
+            var pairs = line.Split(',');
+            var pair1Sections = pairs[0].Split('-').Select(int.Parse).ToArray();
+            var pair2Sections = pairs[1].Split('-').Select(int.Parse).ToArray();
+            if (pair1Sections[0] >= pair2Sections[0] && pair1Sections[1] <= pair2Sections[1])
+            {
+                containedCount++;
+            }
+            else if (pair2Sections[0] >= pair1Sections[0] && pair2Sections[1] <= pair1Sections[1])
+            {
+                containedCount++;
+            }
+        }
+        return containedCount;
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day04Tests.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode._2022;
+using Xunit;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day04Tests
+{
+    [Fact]
+    public void CampCleanupPart1_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "2-4,6-8\r\n2-3,4-5\r\n5-7,7-9\r\n2-8,3-7\r\n6-6,4-6\r\n2-6,4-8";
+        const int expectedFullyContainedPairs = 2;
+
+        // Act
+        var result = Day04.CampCleanupPart1(input);
+
+        // Assert
+        Assert.Equal(expectedFullyContainedPairs, result);
+    }
+    
+}
