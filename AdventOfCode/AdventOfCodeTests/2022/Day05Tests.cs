54345ba44def49dc0147f9ae1d6c3b64fc07d392
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-05 11:21:09 +0100

    Solve day 5 part 2

--- AdventOfCode/AdventOfCodeTests/2022/Day05Tests.cs
+++ AdventOfCode/AdventOfCodeTests/2022/Day05Tests.cs
@@ -18,4 +18,18 @@ public class Day05Tests
         // Assert
         Assert.Equal(expectedTopCrates, result);
     }
+
+    [Fact]
+    public void SupplyStacksPart2_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "    [D]    \r\n[N] [C]    \r\n[Z] [M] [P]\r\n 1   2   3 \r\n\r\nmove 1 from 2 to 1\r\nmove 3 from 1 to 3\r\nmove 2 from 2 to 1\r\nmove 1 from 1 to 2";
+        const string expectedTopCrates = "MCD";
+
+        // Act
+        var result = Day05Part2.SupplyStacks(input);
+        
+        // Assert
+        Assert.Equal(expectedTopCrates, result);
+    }
 }
