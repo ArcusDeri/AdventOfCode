e5e708729ce28f3dccf06ffb5a2beb9fc8c1a1af
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-05 11:20:54 +0100

    Solve day 5 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day05Part1.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+
+namespace AdventOfCode._2022;
+
+public static class Day05Part1
+{
+    public static string SupplyStacks(string input)
+    {
+        var inputParts = input.Split("\r\n\r\n");
+        var cratesSetupLines = inputParts[0].Split("\r\n");
+        var rearrangementInstructions = ResolveRearrangementInstructions(inputParts[1]);
+        var stackMap = ResolveStackMap(cratesSetupLines);
+
+        foreach (var instruction in rearrangementInstructions)
+        {
+            for (int i = 0; i < instruction.CratesToBeMovedCount; i++)
+            {
+                stackMap[instruction.ToStackId].Push(stackMap[instruction.FromStackId].Pop());
+            }
+        }
+
+        var sb = new StringBuilder();
+        foreach (var stack in stackMap.Values)
+        {
+            sb.Append(stack.Peek());
+        }
+
+        return sb.ToString();
+    }
+
+    private static RearrangementInstruction[] ResolveRearrangementInstructions(string instructionsInput)
+    {
+        var instructionLines = instructionsInput.Split("\r\n");
+
+        return instructionLines.Select(instructionLine => instructionLine.Split(" "))
+            .Select(words =>
+                new RearrangementInstruction(int.Parse(words[1]), int.Parse(words[3]), int.Parse(words[5])))
+            .ToArray();
+    }
+
+    private static Dictionary<int, Stack<char>> ResolveStackMap(string[] setupLines)
+    {
+        var stackMap = new Dictionary<int, Stack<char>>();
+
+        // Resolve how many stacks are needed
+        for (int i = 1, j = 1; i < setupLines[0].Length; i += 4, j++)
+        {
+            stackMap[j] = new Stack<char>();
+        }
+
+        // fill the stacks: start from the top so the top crate will be at the bottom - it will be inverted later
+        foreach (var setupLine in setupLines)
+        {
+            for (int i = 1, j = 1; i < setupLine.Length; i += 4, j++)
+            {
+                if (char.IsLetter(setupLine[i]))
+                {
+                    stackMap[j].Push(setupLine[i]);
+                }
+            }
+        }
+
+        // invert the stacks
+        foreach (var stackEntry in stackMap)
+        {
+            var inversionStack = new Stack<char>();
+            while (stackEntry.Value.Any())
+            {
+                inversionStack.Push(stackEntry.Value.Pop());
+            }
+
+            stackMap[stackEntry.Key] = inversionStack;
+        }
+
+        return stackMap;
+    }
+
+    private record RearrangementInstruction(int CratesToBeMovedCount, int FromStackId, int ToStackId);
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day05Tests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using AdventOfCode._2022;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day05Tests
+{
+    [Fact]
+    public void SupplyStacksPart1_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "    [D]    \r\n[N] [C]    \r\n[Z] [M] [P]\r\n 1   2   3 \r\n\r\nmove 1 from 2 to 1\r\nmove 3 from 1 to 3\r\nmove 2 from 2 to 1\r\nmove 1 from 1 to 2";
+        const string expectedTopCrates = "CMZ";
+
+        // Act
+        var result = Day05Part1.SupplyStacks(input);
+        
+        // Assert
+        Assert.Equal(expectedTopCrates, result);
+    }
+}
