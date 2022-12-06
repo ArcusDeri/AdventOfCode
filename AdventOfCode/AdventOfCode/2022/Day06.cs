8d4fed61effc8078934c8698c5b19848d7fb5a46
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-06 20:00:34 +0100

    Solve day 6 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day06.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode._2022;
+
+public static class Day06
+{
+    public static int TuningTroublePart1(string input)
+    {
+        var charMap = new Dictionary<char, int>();
+        var charPos = 0;
+        while (charMap.Count < 4)
+        {
+            for (int aheadLookupPos = charPos; aheadLookupPos <  charPos + 4 ; aheadLookupPos++)
+            {
+                if (charMap.ContainsKey(input[aheadLookupPos]))
+                {
+                    charMap.Clear();
+                    break;
+                }
+
+                charMap[input[aheadLookupPos]] = default;
+                if (charMap.Count == 4)
+                {
+                    return aheadLookupPos + 1;
+                }
+            }
+
+            charPos++;
+        }
+
+        throw new Exception("Packet start sequence not found");
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day06Tests.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode._2022;
+using Xunit;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day06Tests
+{
+    [Theory]
+    [InlineData(7, "mjqjpqmgbljsphdztnvjfqwrcgsmlb")]
+    [InlineData(5, "bvwbjplbgvbhsrlpgdmjqwftvncz")]
+    [InlineData(6, "nppdvjthqldpwncqszvftbrmjlhg")]
+    [InlineData(10, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")]
+    [InlineData(11, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")]
+    public void TuningTroublePart1_ShouldReturnExpectedResult(int expectedStartOfMessageIndex, string input)
+    {
+        // Arrange
+        // Act
+        var result = Day06.TuningTroublePart1(input);
+
+        // Assert
+        Assert.Equal(expectedStartOfMessageIndex, result);
+    }
+}
