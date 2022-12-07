7024a4acaee17b125e7382e3b44a56a6040c7838
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-07 23:02:21 +0100

    Solve day 7 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day07.cs
@@ -0,0 +1,95 @@
+﻿namespace AdventOfCode._2022;
+
+public class Day07
+{
+    public static int NoSpaceLeftOnDevicePart1(string input)
+    {
+        var root = ResolveFileSystemTree(input);
+        var stack = new Stack<TreeNode>(new[] {root});
+        var candidateDirSizes = 0;
+        while (stack.Any())
+        {
+            var current = stack.Pop();
+            if (current.Size < 100_000)
+            {
+                candidateDirSizes += current.Size;
+            }
+
+            foreach (var dir in current.Directories.Values)
+            {
+                stack.Push(dir);
+            }
+        }
+
+        return candidateDirSizes;
+    }
+
+    private static TreeNode ResolveFileSystemTree(string input)
+    {
+        var root = new TreeNode("/");
+        var currentNode = root;
+        var inputLines = input.Split("\r\n");
+
+        for (int i = 1; i < inputLines.Length; i++)
+        {
+            var currentLine = inputLines[i];
+            if (currentLine == "$ ls")
+            {
+                continue;
+            }
+
+            if (currentLine.StartsWith("dir"))
+            {
+                var dirName = currentLine[4..];
+                currentNode.Directories[dirName] = new TreeNode(dirName, currentNode);
+            }
+            else if (char.IsDigit(currentLine[0]))
+            {
+                var lineParts = currentLine.Split(" ");
+                var size = int.Parse(lineParts[0]);
+                var name = lineParts[1];
+                currentNode.Size += size;
+                currentNode.Files[name] = new TreeNode(name, currentNode, size);
+                var ancestor = currentNode.Parent;
+                while (ancestor is not null)
+                {
+                    ancestor.Size += size;
+                    ancestor = ancestor.Parent;
+                }
+            }
+            else if (currentLine == "$ cd /")
+            {
+                currentNode = root;
+            }
+            else if (currentLine == "$ cd ..")
+            {
+                currentNode = currentNode.Parent;
+            }
+            else
+            {
+                var nextNodeName = currentLine[5..];
+                currentNode = currentNode.Directories[nextNodeName];
+            }
+        }
+
+        return root;
+    }
+
+    private class TreeNode
+    {
+        public string Name { get; }
+        public Dictionary<string, TreeNode> Directories { get; }
+        public Dictionary<string, TreeNode> Files { get; }
+        public TreeNode? Parent { get; }
+        public int Size { get; set; }
+
+        public TreeNode(string name, TreeNode? parent = null, int size = 0)
+        {
+            Name = name;
+            Directories = new Dictionary<string, TreeNode>();
+            Files = new Dictionary<string, TreeNode>();
+            Parent = parent;
+            Size = size;
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day07Tests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using AdventOfCode._2022;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day07Tests
+{
+    [Fact]
+    public void NoSpaceLeftOnDevicePart1_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        const int expectedCumulativeDirectorySize = 95_437;
+        var input = "$ cd /\r\n$ ls\r\ndir a\r\n14848514 b.txt\r\n8504156 c.dat\r\ndir d\r\n$ cd a\r\n$ ls\r\ndir e\r\n29116 f\r\n2557 g\r\n62596 h.lst\r\n$ cd e\r\n$ ls\r\n584 i\r\n$ cd ..\r\n$ cd ..\r\n$ cd d\r\n$ ls\r\n4060174 j\r\n8033020 d.log\r\n5626152 d.ext\r\n7214296 k";
+
+        // Act
+        var result = Day07.NoSpaceLeftOnDevicePart1(input);
+
+        // Assert
+        Assert.Equal(expectedCumulativeDirectorySize, result);
+    }
+}
