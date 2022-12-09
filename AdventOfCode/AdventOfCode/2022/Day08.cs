669b5b3e3b170b788afb2ba4227aad8e63fffb4b
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-09 20:53:04 +0100

    Solve day 8 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day08.cs
@@ -0,0 +1,99 @@
+﻿namespace AdventOfCode._2022;
+
+public static class Day08
+{
+    public static int TreetopTreeHousePart1(string input)
+    {
+        var lines = input.Split("\r\n");
+        var grid = new int[lines.Length][];
+        var visibleMap = new Dictionary<string, int>();
+        var visibleCount = 0;
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var digits = lines[i].ToCharArray();
+            grid[i] = new int[digits.Length];
+            for (int j = 0; j < digits.Length; j++)
+            {
+                grid[i][j] = int.Parse(digits[j].ToString());
+            }
+        }
+
+        for (int y = 0; y < grid.Length; y++)
+        {
+            for (int x = 0; x < grid[y].Length; x++)
+            {
+                if (x == 0 || x == grid[y].Length - 1 || y == 0 || y == grid.Length - 1)
+                {
+                    visibleCount++;
+                    continue;
+                }
+
+                var currentValue = grid[y][x];
+                var fromLeft = true;
+                for (int i = 0; i < x; i++)
+                {
+                    if (grid[y][i] >= currentValue)
+                    {
+                        fromLeft = false;
+                        break;
+                    }
+                }
+
+                if (fromLeft)
+                {
+                    visibleCount++;
+                    continue;
+                }
+
+                var fromRight = true;
+                for (int i = grid[y].Length - 1; i > x; i--)
+                {
+                    if (grid[y][i] >= currentValue)
+                    {
+                        fromRight = false;
+                        break;
+                    }
+                }
+
+                if (fromRight)
+                {
+                    visibleCount++;
+                    continue;
+                }
+
+                var fromTop = true;
+                for (int i = 0; i < y; i++)
+                {
+                    if (grid[i][x] >= currentValue)
+                    {
+                        fromTop = false;
+                        break;
+                    }
+                }
+
+                if (fromTop)
+                {
+                    visibleCount++;
+                    continue;
+                }
+
+                var fromBottom = true;
+                for (int i = grid.Length - 1; i > y; i--)
+                {
+                    if (grid[i][x] >= currentValue)
+                    {
+                        fromBottom = false;
+                        break;
+                    }
+                }
+
+                if (fromBottom)
+                {
+                    visibleCount++;
+                }
+            }
+        }
+        return visibleCount;
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day08Tests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using AdventOfCode._2022;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day08Tests
+{
+    [Fact]
+    public void TreetopTreeHousePart1_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        const int expectedResult = 21;
+        var input = "30373\r\n25512\r\n65332\r\n33549\r\n35390";
+
+        // Act
+        var result = Day08.TreetopTreeHousePart1(input);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
