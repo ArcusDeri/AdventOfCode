cdc3ebe097541a98dbe841537c7fccb06ecf1ab9
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-14 18:15:05 +0100

    Solve day 9 part 2, fix part 1

--- AdventOfCode/AdventOfCode/2022/Day09.cs
+++ AdventOfCode/AdventOfCode/2022/Day09.cs
@@ -27,24 +27,73 @@ public static class Day09
                     headPos.Y += yStepSize;
                 }
 
-                var yDiff = Math.Abs(headPos.Y - tailPos.Y);
-                var xDiff = Math.Abs(headPos.X - tailPos.X);
-
-                if (yDiff == 1 && xDiff == 2 || yDiff == 2 && xDiff == 1)
+                if (Math.Abs(tailPos.X - headPos.X) <= 1 && Math.Abs(tailPos.Y - headPos.Y) <= 1)
                 {
-                    tailPos.X += headPos.X > tailPos.X ? 1 : -1;
-                    tailPos.Y += headPos.Y > tailPos.Y ? 1 : -1;
-                }
-                else if (yDiff > 1)
-                {
-                    tailPos.Y += yStepSize;
-                }
-                else if (xDiff > 1)
-                {
-                    tailPos.X += xStepSize;
+                    continue;
                 }
 
+                var yDiff = Math.Sign(tailPos.Y - headPos.Y);
+                var xDiff = Math.Sign(tailPos.X - headPos.X);
+                tailPos.X -= xDiff;
+                tailPos.Y -= yDiff;
                 visitedMap[$"X{tailPos.X}Y{tailPos.Y}"] = 1;
+            }
+        }
+        return visitedMap.Count;
+    }
+
+    public static int RopeBridgePart2(string input)
+    {
+        var lines = input.Split("\r\n");
+        var visitedMap = new Dictionary<string, int> {{"X0Y0", 1}};
+        var knots = new[]
+        {
+            new Point(0, 0),
+            new Point(0, 0),
+            new Point(0, 0),
+            new Point(0, 0),
+            new Point(0, 0),
+            new Point(0, 0),
+            new Point(0, 0),
+            new Point(0, 0),
+            new Point(0, 0),
+            new Point(0, 0),
+        };
+        var headPos = knots[0];
+
+        foreach (var instruction in lines)
+        {
+            var instructionPart = instruction.Split(" ");
+            var direction = instructionPart[0];
+            var steps = int.Parse(instructionPart[1]);
+            var xStepSize = direction == "L" ? -1 : 1;
+            var yStepSize = direction == "D" ? -1 : 1;
+
+            for (int i = 0; i < steps; i++)
+            {
+                if (direction is "R" or "L")
+                {
+                    headPos.X += xStepSize;
+                }
+                else
+                {
+                    headPos.Y += yStepSize;
+                }
+
+                 
+                for (int j = 1; j < knots.Length; j++)
+                {
+                    if (Math.Abs(knots[j].X - knots[j - 1].X) <= 1 && Math.Abs(knots[j].Y - knots[j - 1].Y) <= 1)
+                    {
+                        continue;
+                    }
+                    var yDiff = Math.Sign(knots[j].Y - knots[j - 1].Y);
+                    var xDiff = Math.Sign(knots[j].X - knots[j - 1].X);
+
+                    knots[j].Y -= yDiff;
+                    knots[j].X -= xDiff;
+                }
+                visitedMap[$"X{knots[9].X}Y{knots[9].Y}"] = 1;
             }
         }
         return visitedMap.Count;
--- AdventOfCode/AdventOfCodeTests/2022/Day09Tests.cs
+++ AdventOfCode/AdventOfCodeTests/2022/Day09Tests.cs
@@ -18,4 +18,18 @@ public class Day09Tests
         // Assert
         Assert.Equal(expectedPositionsCount, result);
     }
+
+    [Fact]
+    public void RopeBridgePart2_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "R 5\r\nU 8\r\nL 8\r\nD 3\r\nR 17\r\nD 10\r\nL 25\r\nU 20";
+        const int expectedPositionsCount = 36;
+
+        // Act
+        var result = Day09.RopeBridgePart2(input);
+
+        // Assert
+        Assert.Equal(expectedPositionsCount, result);
+    }
 }
