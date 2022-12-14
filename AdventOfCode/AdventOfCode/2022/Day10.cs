cb4e4f4d1da180477df7bf2ae8b0f7169a8f80d9
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-14 18:44:03 +0100

    Solve day 10 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day10.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode._2022;
+
+public class Day10
+{
+    public static int CathodeRayTubePart1(string input)
+    {
+        var instructionLines = input.Split("\r\n");
+        var x = 1;
+        var cycleCount = 0;
+        var result = 0;
+        var readAtCycle = 20;
+
+        foreach (var instructionLine in instructionLines)
+        {
+            if (instructionLine == "noop")
+            {
+                cycleCount++;
+                if (cycleCount == readAtCycle && readAtCycle < 221)
+                {
+                    result += cycleCount * x;
+                    readAtCycle += 40;
+                }
+                continue;
+            }
+
+            var addValue = int.Parse(instructionLine.Split(" ")[1]);
+            for (int i = 0; i < 2; i++)
+            {
+                cycleCount++;
+                
+                if (cycleCount == readAtCycle && readAtCycle < 221)
+                {
+                    result += cycleCount * x;
+                    readAtCycle += 40;
+                }
+            }
+
+            x += addValue;
+        }
+        
+        return result;
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day10Tests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using AdventOfCode._2022;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day10Tests
+{
+    [Fact]
+    public void CathodeRayTubePart1_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var input = "addx 15\r\naddx -11\r\naddx 6\r\naddx -3\r\naddx 5\r\naddx -1\r\naddx -8\r\naddx 13\r\naddx 4\r\nnoop\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx -35\r\naddx 1\r\naddx 24\r\naddx -19\r\naddx 1\r\naddx 16\r\naddx -11\r\nnoop\r\nnoop\r\naddx 21\r\naddx -15\r\nnoop\r\nnoop\r\naddx -3\r\naddx 9\r\naddx 1\r\naddx -3\r\naddx 8\r\naddx 1\r\naddx 5\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx -36\r\nnoop\r\naddx 1\r\naddx 7\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\naddx 6\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx 7\r\naddx 1\r\nnoop\r\naddx -13\r\naddx 13\r\naddx 7\r\nnoop\r\naddx 1\r\naddx -33\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\nnoop\r\nnoop\r\nnoop\r\naddx 8\r\nnoop\r\naddx -1\r\naddx 2\r\naddx 1\r\nnoop\r\naddx 17\r\naddx -9\r\naddx 1\r\naddx 1\r\naddx -3\r\naddx 11\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx -13\r\naddx -19\r\naddx 1\r\naddx 3\r\naddx 26\r\naddx -30\r\naddx 12\r\naddx -1\r\naddx 3\r\naddx 1\r\nnoop\r\nnoop\r\nnoop\r\naddx -9\r\naddx 18\r\naddx 1\r\naddx 2\r\nnoop\r\nnoop\r\naddx 9\r\nnoop\r\nnoop\r\nnoop\r\naddx -1\r\naddx 2\r\naddx -37\r\naddx 1\r\naddx 3\r\nnoop\r\naddx 15\r\naddx -21\r\naddx 22\r\naddx -6\r\naddx 1\r\nnoop\r\naddx 2\r\naddx 1\r\nnoop\r\naddx -10\r\nnoop\r\nnoop\r\naddx 20\r\naddx 1\r\naddx 2\r\naddx 2\r\naddx -6\r\naddx -11\r\nnoop\r\nnoop\r\nnoop";
+        const int expectedResult = 13140;
+
+        // Act
+        var result = Day10.CathodeRayTubePart1(input);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
