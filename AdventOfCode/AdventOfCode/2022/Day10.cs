5fd8d944315ee7f9e89d50d870e2b9f9cc4bb083
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-16 15:42:42 +0100

    Solve day 10 part 2

--- AdventOfCode/AdventOfCode/2022/Day10.cs
+++ AdventOfCode/AdventOfCode/2022/Day10.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode._2022;
+﻿using System.Text;
+
+namespace AdventOfCode._2022;
 
 public class Day10
 {
@@ -39,5 +41,55 @@ public class Day10
         }
         
         return result;
+    }
+
+    public static string CathodeRayTubePart2(string input)
+    {
+        var instructionLines = input.Split("\r\n");
+        var x = 1;
+        var sb = new StringBuilder();
+        var instructionIndex = 0;
+        var cycle = 0;
+
+        while (cycle < 240)
+        {
+            if (instructionLines[instructionIndex] == "noop")
+            {
+                DrawPixel();
+
+                instructionIndex++;
+                cycle++;
+                continue;
+            }
+
+            DrawPixel();
+            cycle++;
+            var addValue = int.Parse(instructionLines[instructionIndex].Split(" ")[1]);
+
+            DrawPixel();
+            cycle++;
+
+            x += addValue;
+            instructionIndex++;
+        }
+
+        for (int i = 1; i < 6; i++)
+        {
+            sb.Insert(40 * i + 2 * (i - 1), "\r\n");
+        }
+
+        void DrawPixel()
+        {
+            var normalizedCycle = cycle % 40;
+            if (x - 1 <= normalizedCycle && normalizedCycle <= x + 1)
+            {
+                sb.Append('#');
+            }
+            else
+            {
+                sb.Append('.');
+            }
+        } 
+        return sb.ToString();
     }
 }
