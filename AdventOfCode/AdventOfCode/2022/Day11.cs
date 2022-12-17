426314de53a15a518ee1ff9769281adc8cdbcef9
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-17 17:45:30 +0100

    Solve day 11 part 1

---
+++ AdventOfCode/AdventOfCode/2022/Day11.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode._2022;
+
+public static class Day11
+{
+    public static int MonkeyInTheMiddlePart1(Dictionary<int, Monkey> monkeys)
+    {
+        var roundCount = 1;
+        while (roundCount < 21)
+        {
+            for (int i = 0; i < monkeys.Count; i++)
+            {
+                var monkey = monkeys[i];
+                while (monkey.Items.Any())
+                {
+                    var examinationResult = monkey.Examine();
+                    var throwTo = monkey.ResolveThrowTo(examinationResult);
+                    monkeys[throwTo].Items.Enqueue(examinationResult);
+                }
+            }
+            roundCount++;
+        }
+
+        var max1 = 0;
+        var max2 = 0;
+        for (int i = 0; i < monkeys.Count; i++)
+        {
+            if (monkeys[i].InspectedCount > max1)
+            {
+                max2 = max1;
+                max1 = monkeys[i].InspectedCount;
+                continue;
+            }
+
+            if (monkeys[i].InspectedCount > max2)
+            {
+                max2 = monkeys[i].InspectedCount;
+            }
+        }
+
+        return max1 * max2;
+    }
+}
+
+public class Monkey
+{
+    public Queue<int> Items { get; init; }
+    public Func<int, int> ResolveThrowTo { get; init; }
+    public Func<int, int> OperationStrategy { get; init; }
+    public int InspectedCount { private set; get; }
+
+    public int Examine()
+    {
+        InspectedCount++;
+        var old = Items.Dequeue();
+        return OperationStrategy(old) / 3;
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/2022/Day11Tests.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using AdventOfCode._2022;
+using Xunit;
+
+namespace AdventOfCodeTests._2022;
+
+public class Day11Tests
+{
+    [Fact]
+    public void MonkeyInTheMiddlePart1_ShouldReturnExpectedResultForDemoData()
+    {
+        // Arrange
+        const int expectedResult = 10605;
+        var monkeys = new Dictionary<int, Monkey>
+        {
+            {
+                0,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {79, 98}),
+                    OperationStrategy = old => old * 19,
+                    ResolveThrowTo = val => val % 23 == 0 ? 2 : 3,
+                }
+            },
+            {
+                1,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {54, 65, 75, 74}),
+                    OperationStrategy = old => old + 6,
+                    ResolveThrowTo = val => val % 19 == 0 ? 2 : 0,
+                }
+            },
+            {
+                2,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {79, 60, 97}),
+                    OperationStrategy = old => old * old,
+                    ResolveThrowTo = val => val % 13 == 0 ? 1 : 3,
+                }
+            },
+            {
+                3,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {74}),
+                    OperationStrategy = old => old + 3,
+                    ResolveThrowTo = val => val % 17 == 0 ? 0 : 1,
+                }
+            },
+        };
+        // Act
+        var result = Day11.MonkeyInTheMiddlePart1(monkeys);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void MonkeyInTheMiddlePart1_ShouldReturnExpectedResultForActualData()
+    {
+        // Arrange
+        var monkeys = new Dictionary<int, Monkey>
+        {
+            {
+                0,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {83, 88, 96, 79, 86, 88, 70}),
+                    OperationStrategy = old => old * 5,
+                    ResolveThrowTo = val => val % 11 == 0 ? 2 : 3,
+                }
+            },
+            {
+                1,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {59, 63, 98, 85, 68, 72}),
+                    OperationStrategy = old => old * 11,
+                    ResolveThrowTo = val => val % 5 == 0 ? 4 : 0,
+                }
+            },
+            {
+                2,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {90, 79, 97, 52, 90, 94, 71, 70}),
+                    OperationStrategy = old => old + 2,
+                    ResolveThrowTo = val => val % 19 == 0 ? 5 : 6,
+                }
+            },
+            {
+                3,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {97, 55, 62}),
+                    OperationStrategy = old => old + 5,
+                    ResolveThrowTo = val => val % 13 == 0 ? 2 : 6,
+                }
+            },
+            {
+                4,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {74, 54, 94, 76}),
+                    OperationStrategy = old => old * old,
+                    ResolveThrowTo = val => val % 7 == 0 ? 0 : 3,
+                }
+            },
+            {
+                5,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {58}),
+                    OperationStrategy = old => old + 4,
+                    ResolveThrowTo = val => val % 17 == 0 ? 7 : 1,
+                }
+            },
+            {
+                6,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {66, 63}),
+                    OperationStrategy = old => old + 6,
+                    ResolveThrowTo = val => val % 2 == 0 ? 7 : 5,
+                }
+            },
+            {
+                7,
+                new Monkey
+                {
+                    Items = new Queue<int>(new[] {56, 56, 90, 96, 68}),
+                    OperationStrategy = old => old + 7,
+                    ResolveThrowTo = val => val % 3 == 0 ? 4 : 1,
+                }
+            },
+        };
+        // Act
+        var result = Day11.MonkeyInTheMiddlePart1(monkeys);
+
+        // Assert
+        Assert.True(result > 0);
+    }
+}
