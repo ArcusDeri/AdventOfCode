f4ee741adae065b114535dc353ad88421158ec1c
Marcin Michniak <marcin.michniak@fortedigital.no>
2022-12-19 16:32:59 +0100

    Solve day 11 part 2

--- AdventOfCode/AdventOfCode/2022/Day11.cs
+++ AdventOfCode/AdventOfCode/2022/Day11.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode._2022;
+﻿using System.Numerics;
+
+namespace AdventOfCode._2022;
 
 public static class Day11
 {
@@ -39,6 +41,54 @@ public static class Day11
 
         return max1 * max2;
     }
+
+    public static long MonkeyInTheMiddlePart2(Dictionary<int, DifficultMonkey> monkeys)
+    {
+        var roundCount = 1;
+        var divisors = new List<int>();
+        foreach (var monkey in monkeys)
+        {
+            divisors.Add(monkey.Value.Divisor);
+        }
+
+        var lowestCommonMultiple = divisors.Aggregate((val, next) => val * next / GetGreatestCommonDivisor(val, next));
+
+        while (roundCount < 10001)
+        {
+            for (int i = 0; i < monkeys.Count; i++)
+            {
+                var monkey = monkeys[i];
+                while (monkey.Items.Any())
+                {
+                    var examinationResult = monkey.Examine() % lowestCommonMultiple;
+                    var throwTo = monkey.ResolveThrowTo(examinationResult);
+                    monkeys[throwTo].Items.Enqueue(examinationResult);
+                }
+            }
+            roundCount++;
+        }
+
+        var max1 = 0L;
+        var max2 = 0L;
+        for (int i = 0; i < monkeys.Count; i++)
+        {
+            if (monkeys[i].InspectedCount > max1)
+            {
+                max2 = max1;
+                max1 = monkeys[i].InspectedCount;
+                continue;
+            }
+
+            if (monkeys[i].InspectedCount > max2)
+            {
+                max2 = monkeys[i].InspectedCount;
+            }
+        }
+
+        return max1 * max2;
+    }
+
+    private static int GetGreatestCommonDivisor(int x, int y) => y == 0 ? x : GetGreatestCommonDivisor(y, x % y);
 }
 
 public class Monkey
@@ -53,5 +103,22 @@ public class Monkey
         InspectedCount++;
         var old = Items.Dequeue();
         return OperationStrategy(old) / 3;
+    }
+}
+
+public class DifficultMonkey
+{
+    public Queue<long> Items { get; init; }
+    public Func<long, int> ResolveThrowTo { get; init; }
+    public Func<long, long> OperationStrategy { get; init; }
+    public long InspectedCount { private set; get; }
+    public int Divisor { get; set; }
+
+    public long Examine()
+    {
+        InspectedCount++;
+        var old = Items.Dequeue();
+        var x = OperationStrategy(old);
+        return x;
     }
 }
--- AdventOfCode/AdventOfCodeTests/2022/Day11Tests.cs
+++ AdventOfCode/AdventOfCodeTests/2022/Day11Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Numerics;
 using AdventOfCode._2022;
 using Xunit;
 
@@ -52,6 +53,61 @@ public class Day11Tests
         };
         // Act
         var result = Day11.MonkeyInTheMiddlePart1(monkeys);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void MonkeyInTheMiddlePart2_ShouldReturnExpectedResultForDemoData()
+    {
+        // Arrange
+        var expectedResult = 2713310158L;
+        var monkeys = new Dictionary<int, DifficultMonkey>
+        {
+            {
+                0,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {79L, 98L}),
+                    OperationStrategy = old => old * 19,
+                    ResolveThrowTo = val => val % 23 == 0 ? 2 : 3,
+                    Divisor = 23,
+                }
+            },
+            {
+                1,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {54L, 65L, 75L, 74L}),
+                    OperationStrategy = old => old + 6,
+                    ResolveThrowTo = val => val % 19 == 0 ? 2 : 0,
+                    Divisor = 19,
+                }
+            },
+            {
+                2,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {79L, 60L, 97L}),
+                    OperationStrategy = old => old * old,
+                    ResolveThrowTo = val => val % 13 == 0 ? 1 : 3,
+                    Divisor = 13,
+                }
+            },
+            {
+                3,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {74L}),
+                    OperationStrategy = old => old + 3,
+                    ResolveThrowTo = val => val % 17 == 0 ? 0 : 1,
+                    Divisor = 17,
+                }
+            },
+        };
+        // Act
+        var result = Day11.MonkeyInTheMiddlePart2(monkeys);
 
         // Assert
         Assert.Equal(expectedResult, result);
@@ -138,6 +194,100 @@ public class Day11Tests
         };
         // Act
         var result = Day11.MonkeyInTheMiddlePart1(monkeys);
+
+        // Assert
+        Assert.True(result > 0);
+    }
+
+    [Fact]
+    public void MonkeyInTheMiddlePart2_ShouldReturnExpectedResultForActualData()
+    {
+        // Arrange
+        var monkeys = new Dictionary<int, DifficultMonkey>
+        {
+            {
+                0,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {83L, 88L, 96L, 79L, 86L, 88L, 70L}),
+                    OperationStrategy = old => old * 5,
+                    ResolveThrowTo = val => val % 11 == 0 ? 2 : 3,
+                    Divisor = 11,
+                }
+            },
+            {
+                1,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {59L, 63L, 98L, 85L, 68L, 72L}),
+                    OperationStrategy = old => old * 11,
+                    ResolveThrowTo = val => val % 5 == 0 ? 4 : 0,
+                    Divisor = 5,
+                }
+            },
+            {
+                2,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {90L, 79L, 97L, 52L, 90L, 94L, 71L, 70L}),
+                    OperationStrategy = old => old + 2,
+                    ResolveThrowTo = val => val % 19 == 0 ? 5 : 6,
+                    Divisor = 19,
+                }
+            },
+            {
+                3,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {97L, 55L, 62L}),
+                    OperationStrategy = old => old + 5,
+                    ResolveThrowTo = val => val % 13 == 0 ? 2 : 6,
+                    Divisor = 13,
+                }
+            },
+            {
+                4,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {74L, 54L, 94L, 76L}),
+                    OperationStrategy = old => old * old,
+                    ResolveThrowTo = val => val % 7 == 0 ? 0 : 3,
+                    Divisor = 7,
+                }
+            },
+            {
+                5,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {58L}),
+                    OperationStrategy = old => old + 4,
+                    ResolveThrowTo = val => val % 17 == 0 ? 7 : 1,
+                    Divisor = 17,
+                }
+            },
+            {
+                6,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {66L, 63L}),
+                    OperationStrategy = old => old + 6,
+                    ResolveThrowTo = val => val % 2 == 0 ? 7 : 5,
+                    Divisor = 2,
+                }
+            },
+            {
+                7,
+                new DifficultMonkey
+                {
+                    Items = new Queue<long>(new[] {56L, 56L, 90L, 96L, 68L}),
+                    OperationStrategy = old => old + 7,
+                    ResolveThrowTo = val => val % 3 == 0 ? 4 : 1,
+                    Divisor = 3,
+                }
+            },
+        };
+        // Act
+        var result = Day11.MonkeyInTheMiddlePart2(monkeys);
 
         // Assert
         Assert.True(result > 0);
